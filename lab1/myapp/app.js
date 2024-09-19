d9010a07e1e3dd5ae0cadf3cb53c1b3e635d9f91
nikitafadeev102 <150797378+nikitafadeev102@users.noreply.github.com>
2024-09-19 14:15:58 +0300

    Add files via upload

--- lab1/myapp/app.js
+++ lab1/myapp/app.js
@@ -4,7 +4,7 @@ const app = express();
 app.use('/static', express.static(__dirname + '/public'));
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("мяу");
 });
 app.post("/", (req, res) => {
   res.send("Got a POST request");
@@ -15,6 +15,18 @@ app.put("/user", (req, res) => {
 app.delete("/user", (req, res) => {
   res.send("Got a DELETE request at /user");
 });
+
+app.use("/contact", function (request, response) {
+    response.render("contact", {
+        title: "Мои контакты",
+        emailsVisible: true,
+        emails: ["gavgav@mycorp.com", "mioaw@mycorp.com"],
+        phone: "+1234567890",
+    });
+});
+
+app.set("view engine", "pug");
+
 app.listen(3000, () => {
     console.log("Example app listening on port 3000!");
 });
